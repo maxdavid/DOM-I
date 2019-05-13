138c80edb06f4f8bb4b44cb77cb0ee621a86701a
maxdavid <me@maxdavid.com>
2019-05-13 18:24:10 +0200

    Update JS to construct correct page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const siteContent = {
   "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
+    "nav-item-0": "Services",
+    "nav-item-1": "Product",
+    "nav-item-2": "Vision",
+    "nav-item-3": "Features",
+    "nav-item-4": "About",
+    "nav-item-5": "Contact",
     "img-src": "img/logo.png"
   },
   "cta": {
@@ -40,3 +40,35 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+let nav = document.getElementsByTagName("nav")[0];
+let navLinks = nav.querySelectorAll('a');
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].textContent = siteContent['nav'][`nav-item-${i}`];
+}
+
+let cta = document.getElementsByClassName('cta')[0];
+cta.getElementsByTagName('h1')[0].textContent = siteContent['cta']['h1'];
+cta.getElementsByTagName('button')[0].textContent = siteContent['cta']['button'];
+
+const sectionNames = ['features', 'about', 'services', 'product', 'vision'];
+let mainTextContent = document.getElementsByClassName('text-content');
+for (let i = 0; i < sectionNames.length; i++) {
+  mainTextContent[i].getElementsByTagName('h4')[0].textContent = siteContent['main-content'][`${sectionNames[i]}-h4`];
+  mainTextContent[i].getElementsByTagName('p')[0].textContent = siteContent['main-content'][`${sectionNames[i]}-content`];
+}
+
+let contact = document.getElementsByClassName('contact')[0];
+let contactFields = Object.keys(siteContent['contact']);
+for (let i = 0; i < contact.children.length; i++) {
+  contact.children[i].textContent = siteContent['contact'][contactFields[i]];
+}
+
+let footer = document.getElementsByTagName('footer')[0];
+footer.getElementsByTagName('p')[0].textContent = siteContent['footer']['copyright'];
